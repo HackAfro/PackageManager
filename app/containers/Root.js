88b77d405e58017542c4e07cbcb72ef06851add4
Richard Afro <richyafro@hotmail.com>
2019-04-04 15:46:36 +0100

    root component

---
+++ app/containers/Root.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+import Routes from '../Routes';
+
+export default class Root extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Routes />
+      </Fragment>
+    );
+  }
+}
